5d2f006b354393eacbef8189936e60dc9f515386
NathanHaim <nathan.haim@free.fr>
2017-02-21 17:37:02 +0100

    add constants

---
+++ caracteres.h
@@ -0,0 +1,22 @@
+// constante caracteres
+
+#if ! defined ( CARACTERES_H )
+#define CARACTERES_H
+
+const int TAB = 9;
+const int SPACE = 10;
+const int BACKSPACE = 13;
+const int MIN_INT = 48;
+const int MAX_INT = 57;
+
+
+
+const int O_PARENTH = 40;
+const int F_PARENTH = 41;
+const int MULT = 42;
+const int PLUS = 43;
+const int EXPR = 44;
+const int NUMBER = 45;
+
+
+#endif // if ! defined CARACTERES_H
